78f9afb4baf81c154c6085b7bfddb96789d37582
Giugiu19 <gpirasking@gmail.com>
2014-04-22 14:58:29 +0200

    cpufreq: add wheatley Governor

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -156,11 +156,11 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	cputime64_t idle_time;
 	cputime64_t cur_wall_time;
 	cputime64_t busy_time;
-
+    
 	cur_wall_time = jiffies64_to_cputime64(get_jiffies_64());
 	busy_time = cputime64_add(kstat_cpu(cpu).cpustat.user,
 			kstat_cpu(cpu).cpustat.system);
-
+    
 	busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.irq);
 	busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.softirq);
 	busy_time = cputime64_add(busy_time, kstat_cpu(cpu).cpustat.steal);
@@ -169,17 +169,17 @@ static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
 	idle_time = cputime64_sub(cur_wall_time, busy_time);
 	if (wall)
 		*wall = (cputime64_t)jiffies_to_usecs(cur_wall_time);
-
+    
 	return (cputime64_t)jiffies_to_usecs(idle_time);
 }
 
 static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, wall);
-
+    
 	if (idle_time == -1ULL)
 		return get_cpu_idle_time_jiffy(cpu, wall);
-
+    
 	return idle_time;
 }
 
